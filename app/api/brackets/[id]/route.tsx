443b75fc10b99ce158c3abcb86be0a9ca0f11903
Jackson Schacher <jschacher8@gmail.com>
2026-07-25 11:06:47 -0600

    fix _req typing issue

--- app/api/brackets/[id]/route.tsx
+++ app/api/brackets/[id]/route.tsx
@@ -1,6 +1,18 @@
 import clientPromise from "@/lib/mongodb";
+import { NextRequest } from "next/server";
 
-export async function GET(_req, { params }) {
+interface Params {
+  id: string;
+}
+
+interface RouteContext {
+  params: Promise<Params>;
+}
+
+export async function GET(
+  _req: NextRequest,
+  { params }: RouteContext,
+): Promise<Response> {
   const client = await clientPromise;
   const db = client.db("test");
   const brackets = db.collection("brackets");
